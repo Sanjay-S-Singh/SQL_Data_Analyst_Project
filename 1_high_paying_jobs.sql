175375dbe63776af68498af3e3a4fed1df85f834
Sanjay Singh <157696369+Sanjay-S-Singh@users.noreply.github.com>
2024-04-07 13:26:28 +0200

    Add files via upload

---
+++ 1_high_paying_jobs.sql
@@ -0,0 +1,26 @@
+
+/*
+What are the high paying jobs of Data Analyst?
+    Identifying the top 10 highest paying Data Analyst roles with remote location.
+    Remove jobs postings with null salary cells.
+
+*/
+
+SELECT
+    job_id,
+    name AS company_name,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date :: DATE
+FROM
+    job_postings_fact
+LEFT JOIN company_dim
+ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND 
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10;
